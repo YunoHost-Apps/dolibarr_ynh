e880d94f9e17a0c946ca748759220a0c6798f11c
mastereur <22839524+mastereur@users.noreply.github.com>
2020-09-27 23:06:12 +0200

    Fix #34 - Adding dependences according to https://wiki.dolibarr.org/index.php/Dependencies_and_external_libraries

--- scripts/_common.sh
+++ scripts/_common.sh
@@ -3,6 +3,24 @@
 #=================================================
 # COMMON VARIABLES
 #=================================================
+YNH_PHP_VERSION="7.3"
+
+extra_php_dependencies="php${YNH_PHP_VERSION}-mysql \
+						php${YNH_PHP_VERSION}-imagick \
+						php${YNH_PHP_VERSION}-gd \
+						php${YNH_PHP_VERSION}-mcrypt \
+						php${YNH_PHP_VERSION}-openssl \
+						php${YNH_PHP_VERSION}-mbstring \
+						php${YNH_PHP_VERSION}-soap \
+						php${YNH_PHP_VERSION}-curl \
+						php${YNH_PHP_VERSION}-intl \
+						php${YNH_PHP_VERSION}-opcache \
+						php${YNH_PHP_VERSION}-calendar \
+						php${YNH_PHP_VERSION}-zip \
+						php${YNH_PHP_VERSION}-xml \
+						php${YNH_PHP_VERSION}-gd2 \
+						php${YNH_PHP_VERSION}-fileinfo"
+
 
 #=================================================
 # PERSONAL HELPERS
