44d3996b64cb3e592bfb409adb24df5bf1e35b10
Florian HENRY <florian.henry@open-concept.pro>
2020-09-29 11:12:18 +0200

    check with masterteur update

--- scripts/_common.sh
+++ scripts/_common.sh
@@ -23,10 +23,6 @@ extra_php_dependencies="php${YNH_PHP_VERSION}-mysql \
 # PERSONAL HELPERS
 #=================================================
 
-YNH_PHP_VERSION="7.3"
-
-pkg_dependencies="php${YNH_PHP_VERSION}-curl php${YNH_PHP_VERSION}-xml php${YNH_PHP_VERSION}-gd php${YNH_PHP_VERSION}-zip php${YNH_PHP_VERSION}-intl php${YNH_PHP_VERSION}-mbstring"
-
 #=================================================
 # EXPERIMENTAL HELPERS
 #=================================================
