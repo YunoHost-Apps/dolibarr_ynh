5bce9d9409ec66fe5f4bcda6561af93dfce8e489
Éric Gaspar <46165813+ericgaspar@users.noreply.github.com>
2023-05-22 08:30:20 +0200

    v2

--- scripts/_common.sh
+++ scripts/_common.sh
@@ -3,24 +3,6 @@
 #=================================================
 # COMMON VARIABLES
 #=================================================
-#REMOVEME? YNH_PHP_VERSION="8.1"
-
-#REMOVEME? php_dependencies="php$YNH_PHP_VERSION-mysql \
-						php$YNH_PHP_VERSION-imagick \
-						php$YNH_PHP_VERSION-gd \
-						php$YNH_PHP_VERSION-mbstring \
-						php$YNH_PHP_VERSION-soap \
-						php$YNH_PHP_VERSION-curl \
-						php$YNH_PHP_VERSION-intl \
-						php$YNH_PHP_VERSION-opcache \
-						php$YNH_PHP_VERSION-calendar \
-						php$YNH_PHP_VERSION-zip \
-						php$YNH_PHP_VERSION-xml \
-						php$YNH_PHP_VERSION-fileinfo \
-						php$YNH_PHP_VERSION-imap"
-
-# dependencies used by the app (must be on a single line)
-#REMOVEME? pkg_dependencies="$php_dependencies"
 
 #=================================================
 # PERSONAL HELPERS
