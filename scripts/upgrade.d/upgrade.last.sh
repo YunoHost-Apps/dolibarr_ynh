b9c2217d58295773dde4d24de0c92e78459133ea
Félix Piédallu <felix@piedallu.me>
2024-01-24 13:32:26 +0100

    Cleanup legacy update procedure

--- scripts/upgrade.d/upgrade.last.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-# Target version of the Dolibarr update
-next_version="18.0.4"
-
-# Dolibarr tarball checksum
-dolibarr_source_sha256="b1467797f892f72aa313f69f16d5277b481f2a7264ebe1d0ccf43311ca473684"
