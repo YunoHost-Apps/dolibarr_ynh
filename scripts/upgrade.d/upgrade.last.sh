6232d8f7cdb83139302fb553729b2d287ceb73b8
Éric Gaspar <46165813+ericgaspar@users.noreply.github.com>
2024-02-06 12:10:26 +0100

    Update upgrade.last.sh

--- scripts/upgrade.d/upgrade.last.sh
+++ scripts/upgrade.d/upgrade.last.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 # Target version of the Dolibarr update
-next_version="18.0.3"
+next_version="18.0.4"
 
 # Dolibarr tarball checksum
-dolibarr_source_sha256="8639d466b26f0fba7d2b610a96ffcf36a657b1e00bdc2e5a1ac5287521dcb4ed"
+dolibarr_source_sha256="b1467797f892f72aa313f69f16d5277b481f2a7264ebe1d0ccf43311ca473684"
