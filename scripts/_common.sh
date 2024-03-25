20459e3eba624fc8d0a815003d14d3ee83a74536
Félix Piédallu <felix@piedallu.me>
2024-03-25 21:26:07 +0100

    First manifestv2 retry

--- scripts/_common.sh
+++ scripts/_common.sh
@@ -3,24 +3,6 @@
 #=================================================
 # COMMON VARIABLES
 #=================================================
-YNH_PHP_VERSION="8.1"
-
-php_dependencies="php$YNH_PHP_VERSION-mysql \
-						php$YNH_PHP_VERSION-imagick \
-						php$YNH_PHP_VERSION-gd \
-						php$YNH_PHP_VERSION-mbstring \
-						php$YNH_PHP_VERSION-soap \
-						php$YNH_PHP_VERSION-curl \
-						php$YNH_PHP_VERSION-intl \
-						php$YNH_PHP_VERSION-opcache \
-						php$YNH_PHP_VERSION-calendar \
-						php$YNH_PHP_VERSION-zip \
-						php$YNH_PHP_VERSION-xml \
-						php$YNH_PHP_VERSION-fileinfo \
-						php$YNH_PHP_VERSION-imap"
-
-# dependencies used by the app (must be on a single line)
-pkg_dependencies="$php_dependencies"
 
 #=================================================
 # PERSONAL HELPERS
