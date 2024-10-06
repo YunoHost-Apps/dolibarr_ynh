5d4292171ac0407bb99ebf5763f821cad3bb80d0
Éric Gaspar <46165813+ericgaspar@users.noreply.github.com>
2024-10-06 11:56:07 +0200

    cleaning

--- scripts/_common.sh
+++ scripts/_common.sh
@@ -7,8 +7,8 @@
 upgrade_dolibarr() {
     ynh_setup_source --source_id="$source_id" --dest_dir="$install_dir" --full_replace \
         --keep="htdocs/conf/conf.php htdocs/install/install.forced.php"
-    #REMOVEME? Assuming the install dir is setup using ynh_setup_source, the proper chmod/chowns are now already applied and it shouldn't be necessary to tweak perms | chmod -R o-rwx "$install_dir"
-    #REMOVEME? Assuming the install dir is setup using ynh_setup_source, the proper chmod/chowns are now already applied and it shouldn't be necessary to tweak perms | chown -R "$app:www-data" "$install_dir"
+        chmod -R o-rwx "$install_dir"
+        chown -R "$app:www-data" "$install_dir"
     # Remove the lock if it exists
     lock=$install_dir/documents/install.lock
     if [ -f "$lock" ]; then
