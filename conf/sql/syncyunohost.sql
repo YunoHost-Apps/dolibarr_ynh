c1ac9b69e8c0b336b98cf29e839c8a116e87254c
Heidar Ammarloo <132821204+web6463@users.noreply.github.com>
2025-02-16 22:45:37 +0330

    Update syncyunohost.sql

--- conf/sql/syncyunohost.sql
+++ conf/sql/syncyunohost.sql
@@ -10,7 +10,7 @@ INSERT INTO `llx_const` (`name`, `value`, `type`) VALUES
 
 -- Insert new configuration values from user input
 INSERT INTO `llx_const` (`name`, `value`, `type`) VALUES
-('YUNOHOST_BASE_DOMAIN', '{{YUNOHOST_BASE_DOMAIN}}', 'chaine'),
-('YUNOHOST_MAIN_GROUP', '{{YUNOHOST_MAIN_GROUP}}', 'chaine'),
-('YUNOHOST_OLD_MEMBERS', '{{YUNOHOST_OLD_MEMBERS}}', 'chaine')
+('YUNOHOST_BASE_DOMAIN', '{{syncyunohost_base_domain}}', 'chaine'),
+('YUNOHOST_MAIN_GROUP', '{{syncyunohost_main_group}}', 'chaine'),
+('YUNOHOST_OLD_MEMBERS', '{{syncyunohost_old_members}}', 'chaine')
 ON DUPLICATE KEY UPDATE value = VALUES(value);
