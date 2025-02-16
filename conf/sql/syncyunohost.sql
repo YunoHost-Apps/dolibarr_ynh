aa38faa81a8495e42e5f92398fe3b5ec7244f4fc
Heidar Ammarloo <132821204+web6463@users.noreply.github.com>
2025-02-16 16:51:22 +0330

    Create syncyunohost.sql

---
+++ conf/sql/syncyunohost.sql
@@ -0,0 +1,9 @@
+INSERT INTO `llx_const` (`name`, `value`, `type`) VALUES
+('MAIN_FEATURES_LEVEL', '0', 'chaine'),
+('MAIN_MODULE_SYNCYUNOHOST', '1', 'string'),
+('MAIN_MODULE_SYNCYUNOHOST_TRIGGERS', '1', 'chaine'),
+('MAIN_MODULE_SYNCYUNOHOST_ICON', 'fa-file-o', 'chaine'),
+('MAIN_MODULE_ADHERENT', '1', 'string'),
+('ADHERENT_LOGIN_NOT_REQUIRED', '0', 'chaine'),
+('ADHERENT_MAIL_REQUIRED', '1', 'chaine');
+
