570db9d0823b99c6b85802467cdc9c6e36fda7db
Heidar Ammarloo <132821204+web6463@users.noreply.github.com>
2025-02-16 07:56:12 +0330

    Add files via upload

---
+++ sources/extra_files/app/syncyunohost/README.md
@@ -0,0 +1,46 @@
+# SYNCYUNOHOST FOR [DOLIBARR ERP CRM](https://www.dolibarr.org)
+
+## Features
+
+**SyncYunoHost** is a powerful integration module that connects Dolibarr ERP CRM members with YunoHost users. It offers comprehensive user management capabilities, allowing administrators to:
+
+- Automatically synchronize Dolibarr members with YunoHost users.
+- Create new YunoHost users directly from Dolibarr.
+- Enable or disable user accounts.
+- Delete or renew user accounts seamlessly.
+
+<!--
+![Screenshot syncyunohost](img/screenshot_syncyunohost.png?raw=true "SyncYunoHost"){imgmd}
+-->
+
+Other external modules are available on [Dolistore.com](https://www.dolistore.com).
+
+## Translations
+
+Translations can be completed manually by editing files into directories *langs*.
+
+<!--
+This module contains also a sample configuration for Transifex, under the hidden directory [.tx](.tx), so it is possible to manage translation using this service.
+
+For more information, see the [translator's documentation](https://wiki.dolibarr.org/index.php/Translator_documentation).
+
+There is a [Transifex project](https://transifex.com/projects/p/dolibarr-module-template) for this module.
+-->
+### Final steps
+
+From your browser:
+
+  - Log into Dolibarr as a super-administrator
+  - Go to "Setup" -> "Modules"
+  - You should now be able to find and enable the module
+
+## Licenses
+
+### Main code
+
+This module is part of the yunohost package and is distributed under the same license than the dolibarr_ynh package, [AGPL-3.0 license](<https://github.com/Shervin-QZ/dolibarr_ynh/blob/master/LICENSE>).
+
+### Documentation
+
+All texts and readmes are licensed under GFDL.
+
---
+++ sources/extra_files/app/syncyunohost/admin/about.php
@@ -0,0 +1,84 @@
+<?php
+
+// Load Dolibarr environment
+$res = 0;
+// Try main.inc.php into web root known defined into CONTEXT_DOCUMENT_ROOT (not always defined)
+if (!$res && !empty($_SERVER["CONTEXT_DOCUMENT_ROOT"])) {
+	$res = @include $_SERVER["CONTEXT_DOCUMENT_ROOT"]."/main.inc.php";
+}
+// Try main.inc.php into web root detected using web root calculated from SCRIPT_FILENAME
+$tmp = empty($_SERVER['SCRIPT_FILENAME']) ? '' : $_SERVER['SCRIPT_FILENAME']; $tmp2 = realpath(__FILE__); $i = strlen($tmp) - 1; $j = strlen($tmp2) - 1;
+while ($i > 0 && $j > 0 && isset($tmp[$i]) && isset($tmp2[$j]) && $tmp[$i] == $tmp2[$j]) {
+	$i--;
+	$j--;
+}
+if (!$res && $i > 0 && file_exists(substr($tmp, 0, ($i + 1))."/main.inc.php")) {
+	$res = @include substr($tmp, 0, ($i + 1))."/main.inc.php";
+}
+if (!$res && $i > 0 && file_exists(dirname(substr($tmp, 0, ($i + 1)))."/main.inc.php")) {
+	$res = @include dirname(substr($tmp, 0, ($i + 1)))."/main.inc.php";
+}
+// Try main.inc.php using relative path
+if (!$res && file_exists("../../main.inc.php")) {
+	$res = @include "../../main.inc.php";
+}
+if (!$res && file_exists("../../../main.inc.php")) {
+	$res = @include "../../../main.inc.php";
+}
+if (!$res) {
+	die("Include of main fails");
+}
+
+// Libraries
+require_once DOL_DOCUMENT_ROOT.'/core/lib/admin.lib.php';
+require_once DOL_DOCUMENT_ROOT.'/core/lib/functions2.lib.php';
+require_once '../lib/syncyunohost.lib.php';
+
+// Translations
+$langs->loadLangs(array("errors", "admin", "syncyunohost@syncyunohost"));
+
+// Access control
+if (!$user->admin) {
+	accessforbidden();
+}
+
+// Parameters
+$action = GETPOST('action', 'aZ09');
+$backtopage = GETPOST('backtopage', 'alpha');
+
+
+/*
+ * Actions
+ */
+
+// None
+
+
+/*
+ * View
+ */
+
+$form = new Form($db);
+
+$help_url = '';
+$page_name = "SyncYunoHostAbout";
+
+llxHeader('', $langs->trans($page_name), $help_url, '', 0, 0, '', '', '', 'mod-syncyunohost page-admin_about');
+
+// Subheader
+$linkback = '<a href="'.($backtopage ? $backtopage : DOL_URL_ROOT.'/admin/modules.php?restore_lastsearch_values=1').'">'.$langs->trans("BackToModuleList").'</a>';
+
+print load_fiche_titre($langs->trans($page_name), $linkback, 'title_setup');
+
+// Configuration header
+$head = syncyunohostAdminPrepareHead();
+print dol_get_fiche_head($head, 'about', $langs->trans($page_name), 0, 'syncyunohost@syncyunohost');
+
+dol_include_once('/syncyunohost/core/modules/modSyncYunoHost.class.php');
+$tmpmodule = new modSyncYunoHost($db);
+print $tmpmodule->getDescLong();
+
+// Page end
+print dol_get_fiche_end();
+llxFooter();
+$db->close();
