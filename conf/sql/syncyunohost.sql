1349b10fc83d39564bdebef9e9787a7fcc69354f
Heidar Ammarloo <132821204+web6463@users.noreply.github.com>
2025-02-16 23:35:43 +0330

    Update syncyunohost.sql

--- conf/sql/syncyunohost.sql
+++ conf/sql/syncyunohost.sql
@@ -7,3 +7,10 @@ INSERT INTO `llx_const` (`name`, `value`, `type`) VALUES
 ('MAIN_MODULE_ADHERENT', '1', 'string'),
 ('ADHERENT_LOGIN_NOT_REQUIRED', '0', 'chaine'),
 ('ADHERENT_MAIL_REQUIRED', '1', 'chaine');
+
+-- Insert new configuration values from user input
+INSERT INTO `llx_const` (`name`, `value`, `type`) VALUES
+('YUNOHOST_BASE_DOMAIN', '{{syncyunohost_base_domain}}', 'chaine'),
+('YUNOHOST_MAIN_GROUP', '{{syncyunohost_main_group}}', 'chaine'),
+('YUNOHOST_OLD_MEMBERS', '{{syncyunohost_old_members}}', 'chaine')
+ON DUPLICATE KEY UPDATE value = VALUES(value);
