99e95b43ed9c7e8235b40f92d2f6921a79f5e134
Heidar Ammarloo <132821204+web6463@users.noreply.github.com>
2025-02-19 14:05:53 +0330

    Update syncyunohost.sql

--- conf/sql/syncyunohost.sql
+++ conf/sql/syncyunohost.sql
@@ -4,7 +4,6 @@ INSERT INTO `llx_const` (`name`, `value`, `type`) VALUES
 ('MAIN_MODULE_SYNCYUNOHOST', '1', 'string'),
 ('MAIN_MODULE_SYNCYUNOHOST_TRIGGERS', '1', 'chaine'),
 ('MAIN_MODULE_SYNCYUNOHOST_ICON', 'fa-file-o', 'chaine'),
-('MAIN_MODULE_ADHERENT', '1', 'string'),
 ('ADHERENT_LOGIN_NOT_REQUIRED', '0', 'chaine'),
 ('ADHERENT_MAIL_REQUIRED', '1', 'chaine'),
 ('YUNOHOST_OLD_MEMBERS', '0', 'chaine');
