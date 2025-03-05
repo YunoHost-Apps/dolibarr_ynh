bfa0b30cac43cdd880455f7188d03343def42682
Heidar Ammarloo <132821204+web6463@users.noreply.github.com>
2025-03-06 02:32:06 +0330

    Update _common.sh

--- scripts/_common.sh
+++ scripts/_common.sh
@@ -86,5 +86,5 @@ syncyunohost_install(){
     syncyunohost_modules_activate
 }
 syncyunohost_modules_activate(){
-    php "$install_dir/scripts/members/syncyunohost-modules.php" --action=activate --modules=modAdherent,modCorn,modSyncyunohost --base_domain=example.com --main_group=admin --old_members=yes;
+    php "$install_dir/scripts/members/syncyunohost-modules.php" --action=activate --modules=modAdherent,modCron,modSyncyunohost --base_domain=$syncyunohost_base_domain --main_group=$syncyunohost_base_domain --old_members=yes;
 }
